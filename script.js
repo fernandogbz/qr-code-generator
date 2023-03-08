2058496d9cc6890e7168aa1029df56c095c436fa
Fernando Jimenez <fgjv06@gmail.com>
2023-03-08 18:55:11 -0300

    JS - const downloadBtn

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const container = document.querySelector(".container");
 const userInput = document.getElementById("userInput");
 
 const submitBtn = document.getElementById("submit");
+
+const downloadBtn = document.getElementById("download");
